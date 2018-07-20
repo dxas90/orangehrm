8db57238eac38833d7c6385476cacef9a2b68e10
Daniel Ramirez Grave de Peralta <dxas90@gmail.com>
2018-07-20 11:14:59 -0400

    Update fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,4 +1,4 @@
-chmod go+w installer symfony/apps/orangehrm/config
+chmod -R go+w installer symfony/apps/orangehrm/config
 chmod -R go+w lib/confs symfony/log symfony/cache lib/logs symfony/config upgrader/cache upgrader/log
 if [ -f installer/log.txt ]
 then
